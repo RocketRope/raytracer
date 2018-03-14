7b813f82ef98ee1e68d1170dcf10c483249506d2
Fredrik <Fredrik.t.Kling@gmail.com>
2018-03-14 22:04:15 +0100

    change main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <algorithm>
+#include "vmath.h"
 
 int main()
 {
-    std::cout << "Hello!" << std::endl;
+    mat2x2 A(1.1432f,  4.3240f,  2345.20f, 46.02f);
+    mat2x2 B = A.inverse();
+
+  
+
+    std::cout << A   << " |A| = " << A.determinant() << std::endl;
+    std::cout << B   << " |B| = " << B.determinant() << std::endl;
+
+    std::cout << A*B << " "       << B*A             << std::endl;
+
+    
     return 0;
 }
